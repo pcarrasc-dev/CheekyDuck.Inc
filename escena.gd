519d67e6e8c2ad3a7cbf54cf49b098aecbc07b81
Mista317 <amanda.chiong@ug.uchile.cl>
2026-04-19 19:38:46 -0400

    [FIX] fixed camera and ball

--- escena.gd
+++ escena.gd
@@ -1,14 +1,13 @@
-extends Node
+extends Node3D
 
-@export var speed: int = 200
-@export var acceleration: float = 4
+@export var ball_scene: PackedScene
+@onready var ball: Node3D = $Ball
+@onready var ball_spawn_point: Node3D = $BallSpawnPoint
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-func _physics_process(delta: float) -> void:
 	pass
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
