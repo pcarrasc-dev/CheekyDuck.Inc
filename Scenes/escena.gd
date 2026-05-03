55dc9962e5efdcb6c9966f53b70272c7cafa18c1
VictorPetersV <victor.peters@ug.uchile.cl>
2026-05-02 20:06:48 -0400

    Add: Se integraró la mesa, las barras, la pelota y los jugadores a la escena principal

--- Scenes/escena.gd
+++ Scenes/escena.gd
@@ -18,8 +18,14 @@ func _on_players_updated() -> void:
 
 func _setup_bars_authority() -> void:
 	Game.sort_players()
-	$Bar.set_multiplayer_authority(Game.players[0].id)
-	$Bar2.set_multiplayer_authority(Game.players[1].id)
+	$FieldV2/Bar.set_multiplayer_authority(Game.players[0].id)
+	$FieldV2/Bar2.set_multiplayer_authority(Game.players[0].id)
+	$FieldV2/Bar3.set_multiplayer_authority(Game.players[0].id)
+	$FieldV2/Bar4.set_multiplayer_authority(Game.players[0].id)
+	$FieldV2/Bar5.set_multiplayer_authority(Game.players[1].id)
+	$FieldV2/Bar6.set_multiplayer_authority(Game.players[1].id)
+	$FieldV2/Bar7.set_multiplayer_authority(Game.players[1].id)
+	$FieldV2/Bar8.set_multiplayer_authority(Game.players[1].id)
 	Debug.log("Bar0 autoridad: %d | Bar1 autoridad: %d" % [
 		Game.players[0].id, Game.players[1].id
 	])
