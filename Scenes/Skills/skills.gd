aead608ba2a609681a89230ae31eabc4dfad4414
Mista317 <amanda.chiong@ug.uchile.cl>
2026-05-15 02:22:32 -0400

    [ADD] added barrier

---
+++ Scenes/Skills/skills.gd
@@ -0,0 +1,17 @@
+extends Node3D
+class_name Skills
+
+@onready var skills: Node3D = $Skills
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("skill"):
+		pass
