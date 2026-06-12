171c9fb9fe58b944e59c0d3084a6551df2c3d292
Amanda Chiong <amanda.chiong@ug.uchile.cl>
2026-06-12 13:09:46 -0400

    [FIX] menu image and player

--- Scenes/Objects/Bar/Bar.gd
+++ Scenes/Objects/Bar/Bar.gd
@@ -3,7 +3,7 @@ extends StaticBody3D
 
 @export var rotation_sensibility: float = 0.006
 @export var traslation_sensibility: float = 0.0025
-@export var traslation_limit: float = 0.3
+@export var traslation_limit: float = 0.15
 @export var team_bar_index: int = 1
 
 var is_selected: bool = false
