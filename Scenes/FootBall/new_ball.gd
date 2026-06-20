4b93489447ea7b1c25434cafb6b2cfbbce259e41
Pablo Carrasco <pi.carrasco@gmail.com>
2026-06-20 13:34:10 -0400

    [FIX] Fisica de la pelota: eliminar move_and_collide, ajustar parametros, corregir signal duplicado

--- Scenes/FootBall/new_ball.gd
+++ Scenes/FootBall/new_ball.gd
@@ -6,41 +6,41 @@ class_name kinetic_ball
 @onready var multiplayer_synchronizer: MultiplayerSynchronizer = $MultiplayerSynchronizer
 
 var last_player_obj: StaticBody3D
-var last_mouse_speed: Vector2 = Vector2(0,0)
-var mouse_speed: Vector2 = Vector2(0,0)
-var mouse_acc: Vector2 = Vector2(0,0)
+var last_mouse_speed: Vector2 = Vector2.ZERO
+var mouse_speed: Vector2 = Vector2.ZERO
+var mouse_acc: Vector2 = Vector2.ZERO
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
+	sleeping = false
+	body_entered.connect(_on_body_entered)
 	if not is_multiplayer_authority():
 		set_physics_process(false)
 		return
 	multiplayer_synchronizer.set_multiplayer_authority(Game.players[0].id)
 
-func _physics_process(delta: float) -> void:
+
+func _on_body_entered(body: Node) -> void:
 	if not is_multiplayer_authority():
 		return
-	if sleeping:
-		linear_velocity= Vector3(1,1,1)
-	var collide = move_and_collide(linear_velocity*delta)
-	var collision_list:Array[Node3D] = get_colliding_bodies()
-	if collision_list:
-		#Debug.log("collide")
-		var bar = collision_list[0] as Player
-		if bar:
-			last_player_obj = bar
-			#Debug.log(last_player_obj)
-			if collide:
-				apply_force(Vector3(mouse_acc[0], 0, mouse_acc[1])*0.0005)
-				last_mouse_speed = mouse_speed
+	var player := body as Player
+	if not player:
+		return
+	last_player_obj = player
+	var force := Vector3(mouse_acc.x, 0.0, mouse_acc.y) * 0.0005
+	if force.length() > 0.01:
+		apply_force(force)
+		last_mouse_speed = mouse_speed
+
 
 func _input(event: InputEvent) -> void:
 	if not is_multiplayer_authority():
 		return
-	var mouse_event = event as InputEventMouseMotion
+	var mouse_event := event as InputEventMouseMotion
 	if mouse_event:
 		mouse_speed = mouse_event.velocity
-		mouse_acc = (mouse_speed-last_mouse_speed)/get_physics_process_delta_time()
-	
+		mouse_acc = (mouse_speed - last_mouse_speed) / get_physics_process_delta_time()
+
+
 func get_player() -> StaticBody3D:
 	return last_player_obj
