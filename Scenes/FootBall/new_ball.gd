ff16d73e99220de659cbc5f0ad6b849612a5db79
Pablo Carrasco <pi.carrasco@gmail.com>
2026-06-20 14:12:09 -0400

    [FIX] Fisicas del balon mejoradas

--- Scenes/FootBall/new_ball.gd
+++ Scenes/FootBall/new_ball.gd
@@ -6,41 +6,51 @@ class_name kinetic_ball
 @onready var multiplayer_synchronizer: MultiplayerSynchronizer = $MultiplayerSynchronizer
 
 var last_player_obj: StaticBody3D
-var last_mouse_speed: Vector2 = Vector2(0,0)
-var mouse_speed: Vector2 = Vector2(0,0)
-var mouse_acc: Vector2 = Vector2(0,0)
+var last_mouse_speed: Vector2 = Vector2.ZERO
+var mouse_speed: Vector2 = Vector2.ZERO
 
-# Called when the node enters the scene tree for the first time.
+const IMPULSE_MULTIPLIER: float = 0.008
+const MIN_IMPULSE: float = 0.1
+
+var _was_colliding: bool = false
+
+
 func _ready() -> void:
 	if not is_multiplayer_authority():
 		set_physics_process(false)
 		return
 	multiplayer_synchronizer.set_multiplayer_authority(Game.players[0].id)
 
-func _physics_process(delta: float) -> void:
+
+func _physics_process(_delta: float) -> void:
 	if not is_multiplayer_authority():
 		return
-	if sleeping:
-		linear_velocity= Vector3(1,1,1)
-	var collide = move_and_collide(linear_velocity*delta)
-	var collision_list:Array[Node3D] = get_colliding_bodies()
-	if collision_list:
-		#Debug.log("collide")
-		var bar = collision_list[0] as Player
-		if bar:
-			last_player_obj = bar
-			#Debug.log(last_player_obj)
-			if collide:
-				apply_force(Vector3(mouse_acc[0], 0, mouse_acc[1])*0.0005)
-				last_mouse_speed = mouse_speed
+
+	var collision_list: Array[Node3D] = get_colliding_bodies()
+	var is_colliding: bool = false
+
+	for body in collision_list:
+		var player := body as Player
+		if player:
+			is_colliding = true
+			last_player_obj = player
+			if not _was_colliding:
+				var impulse := Vector3(mouse_speed.x, 0.0, mouse_speed.y) * IMPULSE_MULTIPLIER
+				if impulse.length() > MIN_IMPULSE:
+					apply_central_impulse(impulse)
+					last_mouse_speed = mouse_speed
+			break
+
+	_was_colliding = is_colliding
+
 
 func _input(event: InputEvent) -> void:
 	if not is_multiplayer_authority():
 		return
-	var mouse_event = event as InputEventMouseMotion
+	var mouse_event := event as InputEventMouseMotion
 	if mouse_event:
 		mouse_speed = mouse_event.velocity
-		mouse_acc = (mouse_speed-last_mouse_speed)/get_physics_process_delta_time()
-	
+
+
 func get_player() -> StaticBody3D:
 	return last_player_obj
