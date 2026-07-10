6753f504d16e366db17a719a503d79d7da3c4513
Pablo Carrasco <pi.carrasco@gmail.com>
2026-07-10 03:34:03 -0400

    [FIX] skill box: RPC call_local sin authority + add_child primero

--- Scenes/Skills/spawner.gd
+++ Scenes/Skills/spawner.gd
@@ -3,7 +3,6 @@ extends Marker3D
 
 signal skill_received
 
-@onready var multiplayer_spawner: MultiplayerSpawner = $MultiplayerSpawner
 @onready var timer: Timer = $Timer
 @export var skill_scene: PackedScene = preload("res://Scenes/Skills/skill_box.tscn")
 @export var amount: int = 1
@@ -11,55 +10,33 @@ signal skill_received
 
 var skill_count = 0
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	if multiplayer.is_server():
 		timer.start(1 / rate)
 		timer.timeout.connect(_on_timeout)
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
-@rpc("call_local")
+
+
 func try_spawn() -> void:
 	if not multiplayer.is_server():
 		return
 	if skill_count >= amount:
 		return
-	var check_radius: float = SkillBox.radius
-	
-	var space_state: PhysicsDirectSpaceState3D = get_world_3d().direct_space_state
-	var params: PhysicsShapeQueryParameters3D = PhysicsShapeQueryParameters3D.new()
-	params.collide_with_areas = true
-	params.collide_with_bodies = false
-	params.collision_mask = 4
-	var t: Transform3D = Transform3D.IDENTITY
-	t.origin = global_position
-	params.transform = t
-	var square_shape: BoxShape3D = BoxShape3D.new()
-	square_shape.size = Vector3.ONE * check_radius
-	params.shape = square_shape
-	var results: Array[Dictionary] = space_state.intersect_shape(params)
-	for result: Dictionary in results:
-		Debug.log("spawner")
-		Debug.log(result.collider.name)
-	if results.size() > 0:
-		return
-	spawn(global_position)
+	spawn_box.rpc()
 
-func spawn(pos: Vector3) -> void:
+
+@rpc("call_local")
+func spawn_box() -> void:
 	var skill_inst: SkillBox = skill_scene.instantiate()
-	skill_inst.global_position = pos
-	Debug.log("spawn")
-	Debug.log(skill_inst.global_position)
-	multiplayer_spawner.add_child(skill_inst, true)
+	add_child(skill_inst)
 	skill_count += 1
 	skill_inst.skill.connect(func (scene: PackedScene, bar: Bar) -> void: skill_received.emit(scene, bar))
 	skill_inst.tree_exited.connect(func() -> void: skill_count -= 1)
 
+
 func _on_timeout() -> void:
-	try_spawn.rpc()
-	pass
+	try_spawn()
